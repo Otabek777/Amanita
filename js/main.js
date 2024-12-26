1b00c04e6a9b242511a72131c26f7331f6efd30e
Otabek <53979293+Otabek777@users.noreply.github.com>
2024-12-26 14:28:34 +0500

    mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(".accordion__title").click(function() {
     $(this).toggleClass("active");
 });
+$(".header__burgir").click(function() {
+    $(this).toggleClass("active");
+    $(".header__navbar").toggleClass("active");
+});
